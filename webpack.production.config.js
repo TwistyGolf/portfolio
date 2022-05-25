387fc0405bd433ed490343f79f1cf483924389d2
Lewis Savage <lewissavage88@gmail.com>
2022-05-25 21:28:46 +0100

    Prod config wasn't set to production

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.ts',
-  mode: 'development',
+  mode: 'production',
   module: {
     rules: [
       {
